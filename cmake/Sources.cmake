9d19f5ed5d15349e52d480d6f76da91b4cb81a49
Julian Guarin <julian.guarin@globant.com>
2018-04-25 21:11:58 -0500

    Go\! Almost there\!

--- cmake/Sources.cmake
+++ cmake/Sources.cmake
@@ -1,6 +1,6 @@
 
 function(SOURCES_INCLUDE)
-	set(SOURCES_HEADERS ${CMAKE_SOURCE_DIR}/src/cpp PARENT_SCOPE)
+	set(SOURCES_HEADERS ${CMAKE_SOURCE_DIR}/src/cpp)
 	include_directories(${SOURCES_HEADERS})
 endfunction()
 
@@ -8,6 +8,6 @@ function(SOURCES_TO_COMPILE PROJECT_NAME QT_RESOURCES)
 	set(SOURCES_CPP
 			${CMAKE_SOURCE_DIR}/src/cpp/main.cpp
 			${CMAKE_SOURCE_DIR}/src/cpp/ColladaXMLHandler.cpp
-			PARENT_SCOPE)
+			)
 	add_executable(${PROJECT_NAME} ${SOURCES_CPP} ${QT_RESOURCES})
 endfunction() 
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
-#include <QCoreApplication>
+//#include <QCoreApplication>
+#include <QGuiApplication>>
+#include <QQmlApplicationEngine>
 
 #include "ColladaXMLHandler.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
+    //QCoreApplication a(argc, argv);
+    QGuiApplication a(argc, argv);
+    QQmlApplicationEngine engine;
     std::cout << "Hello Qt 5.10.0" << std::endl;
 
     QXmlSimpleReader xmlReader;
@@ -19,7 +22,12 @@ int main(int argc, char *argv[])
     xmlReader.setErrorHandler(pHandler);
 
     bool ok = xmlReader.parse(pSource);
-
-
+    
+    auto qurl = QUrl("qrc:/main.qml");
+    if (!qurl.isValid()) return -1;
+    engine.load();
+    if (engine.rootObjects().isEmpty())
+        return -1;
+    
     return a.exec();
 }
