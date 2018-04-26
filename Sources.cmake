3253273d438d867619cbd69779b17bb7c4678eca
IndieLightAndMagic <julian.guarin@globant.com>
2018-04-25 19:36:01 -0500

    Reorganize

--- Sources.cmake
+++ Sources.cmake
@@ -2,13 +2,13 @@
 
 #Sources 
 set(SOURCES_CPP 
-	${CMAKE_SOURCE_DIR}/main.cpp
-	${CMAKE_SOURCE_DIR}/src/ColladaXMLHandler.cpp
+	${CMAKE_SOURCE_DIR}/src/cpp/main.cpp
+	${CMAKE_SOURCE_DIR}/src/cpp/ColladaXMLHandler.cpp
 	PARENT_SCOPE)
 
 
 #Headers
-set(SOURCES_HEADERS ${CMAKE_SOURCE_DIR}/src PARENT_SCOPE)
+set(SOURCES_HEADERS ${CMAKE_SOURCE_DIR}/src/cpp PARENT_SCOPE)
 
 
 
