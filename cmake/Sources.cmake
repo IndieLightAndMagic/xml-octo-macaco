9840cd98eb8658a8cf4d54c39d82ed0a724edb17
IndieLightAndMagic <julian.guarin@globant.com>
2018-04-25 20:13:23 -0500

    Scope issue solved

--- cmake/Sources.cmake
+++ cmake/Sources.cmake
@@ -1,28 +1,13 @@
 
-get_directory_property(hasParent PARENT_DIRECTORY)
-if(hasParent)
-	message(STATUS "Has a parent scope.")
-else()
-	message(STATUS "Doesn't have a parent scope.")
-endif()
-
-#Sources 
-set(SOURCES_CPP 
-	${CMAKE_SOURCE_DIR}/src/cpp/main.cpp
-	${CMAKE_SOURCE_DIR}/src/cpp/ColladaXMLHandler.cpp
-	PARENT_SCOPE)
-
-
-#Headers
-set(SOURCES_HEADERS ${CMAKE_SOURCE_DIR}/src/cpp PARENT_SCOPE)
-
-
-
-
 function(SOURCES_INCLUDE)
+	set(SOURCES_HEADERS ${CMAKE_SOURCE_DIR}/src/cpp PARENT_SCOPE)
 	include_directories(${SOURCES_HEADERS})
 endfunction()
 
 function(SOURCES_TO_COMPILE PROJECT_NAME QT_RESOURCES)
+	set(SOURCES_CPP
+			${CMAKE_SOURCE_DIR}/src/cpp/main.cpp
+			${CMAKE_SOURCE_DIR}/src/cpp/ColladaXMLHandler.cpp
+			PARENT_SCOPE)
 	add_executable(${PROJECT_NAME} ${SOURCES_CPP} ${QT_RESOURCES})
 endfunction() 
