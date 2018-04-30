7a4079509e0c02e549a97a51542138bdf2913028
Julian Guarin <julian.guarin@globant.com>
2018-04-30 17:19:39 -0500

    Parsing XML.

--- src/cpp/ColladaXMLHandler.cpp
+++ src/cpp/ColladaXMLHandler.cpp
@@ -1,6 +1,8 @@
 #include "ColladaXMLHandler.h"
-
+#include <iostream>
 using namespace G;
+
+
 QTextStream cout(stdout, QIODevice::WriteOnly);
 bool ColladaXMLHandler::startDocument(){
 
@@ -11,6 +13,7 @@ bool ColladaXMLHandler::startDocument(){
 bool ColladaXMLHandler::characters(const QString& rText){
 
     QString t = rText;
+    std::cout << rText.toUtf8().data() << std::endl;
     cout << t.remove('\n');
     return true;
 }
@@ -19,6 +22,10 @@ bool ColladaXMLHandler::startElement(const QString& rUriName, const QString& rLo
 
     QString str = QString("\n%1\\%2").arg(m_indent).arg(rName);
     cout << str;
+    auto strdata = str.toUtf8().data();
+    std::cout << strdata << std::endl;
+
+
     if (atts.length()>0){
         QString fieldName = atts.qName(0);
         QString fieldValue = atts.value(0);
